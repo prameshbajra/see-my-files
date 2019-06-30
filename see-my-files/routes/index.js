844c2b6b1890faf6b6adb2776866b7532451ffe1
prameshbajra <pe.messh@gmail.com>
2019-06-30 15:01:28 +0530

    Showing files to UI.

--- see-my-files/routes/index.js
+++ see-my-files/routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
+const fs = require("fs");
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    let filesInsideDirectory = fs.readdirSync(`${__dirname}\\..\\..\\`);
+    res.render('index', { files: filesInsideDirectory });
 });
 
 module.exports = router;
