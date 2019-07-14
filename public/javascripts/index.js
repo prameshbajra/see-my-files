076e2fa4146e41a3396f995c15e0504f80c63f9f
prameshbajra <pe.messh@gmail.com>
2019-07-14 18:43:55 +0530

    Proper folder navigation done ...

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,6 +1,5 @@
 const showFileContent = (file) => {
     const currentQuery = window.location.search.split("=")[1] ? window.location.search.split("=")[1] : ``;
-    console.log(`?path=${currentQuery}/${file}`);
     const filePath = `?path=${currentQuery}/${file}`;
     window.location.href = filePath;
 }
