9ea65b6f179be41c4f2761d1dc3439094af7932d
prameshbajra <pe.messh@gmail.com>
2019-12-02 18:30:24 +0530

    Right click context menu done ...

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -10,3 +10,35 @@ const performFileOperation = (file) => {
     console.log(`${filePath}`);
     window.location.href = filePath;
 }
+
+
+// For right click menu ...
+const menu = document.querySelector(".menu");
+let menuVisible = false;
+$(".column .file").on("contextmenu", (event) => {
+    event.preventDefault();
+    const origin = {
+        left: event.pageX,
+        top: event.pageY
+    };
+    setPosition(origin);
+    return false;
+})
+
+const toggleMenu = command => {
+    menu.style.display = command === "show" ? "block" : "none";
+    menuVisible = !menuVisible;
+};
+
+const setPosition = ({
+    top,
+    left
+}) => {
+    menu.style.left = `${left}px`;
+    menu.style.top = `${top}px`;
+    toggleMenu("show");
+};
+
+window.addEventListener("click", e => {
+    if (menuVisible) toggleMenu("hide");
+});
