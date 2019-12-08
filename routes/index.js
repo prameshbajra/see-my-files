4e1857d2e94d4fc61580a46b55642ebab397ea6d
prameshbajra <pe.messh@gmail.com>
2019-12-08 20:18:37 +0530

    Zip and download working.
    Moving ahead to deploy to npm registry

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 const fs = require("fs");
+const zipLocal = require('zip-local');
 const utility = require("../utility/utility");
 
 /* GET home page. */
@@ -34,7 +35,12 @@ router.get(`/download`, (req, res, next) => {
 /* Download folder as zip. */
 router.get(`/downloadFolderAsZip`, (req, res, next) => {
     const queryPath = req.query[`path`];
-    console.log(`${process.cwd()}${queryPath}`);
+    let zipFilePath = `${process.cwd()}/${queryPath}`;
+    if (queryPath.includes("/")) {
+        zipFilePath = `${process.cwd()}${queryPath}`;
+    }
+    zipLocal.sync.zip(zipFilePath).compress().save(`${zipFilePath}.zip`);
+    res.download(`${zipFilePath}.zip`);
 });
 
 
